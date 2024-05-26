912b6f28f3be0183dd6bb3811d4c56692d68c6c9
XrL8D <rph26031994@gmail.com>
2024-05-26 16:50:10 -0500

    new

---
+++ src/components/assets/js/main.js
@@ -0,0 +1,65 @@
+/*=============== SEARCH ===============*/
+const searchButton = document.getElementById('search-button'),
+      searchClose = document.getElementById('search-close'),
+      searchContent = document.getElementById('search-content')
+
+/* Menu show */
+if(searchButton){
+    searchButton.addEventListener('click', () =>{
+        searchContent.classList.add('show-search')
+    })
+}
+
+/* Menu hidden */
+if(searchClose){
+    searchClose.addEventListener('click', () =>{
+        searchContent.classList.remove('show-search')
+    })
+}
+/*=============== LOGIN ===============*/
+const loginButton = document.getElementById('login-button'),
+      loginClose = document.getElementById('login-close'),
+      loginContent = document.getElementById('login-content')
+
+/* Menu show */
+if(loginButton){
+    loginButton.addEventListener('click', () =>{
+        loginContent.classList.add('show-login')
+    })
+}
+
+/* Menu hidden */
+if(searchClose){
+    loginClose.addEventListener('click', () =>{
+        loginContent.classList.remove('show-login')
+    })
+}
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== HOME SWIPER ===============*/
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor:true,
+    slidesPerView:'auto',
+    centeredSlides:'auto',
+
+    autoplay: {
+        delay:3000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints:{
+        1220:{
+            spaceBetween:-32,
+        }
+    }
+  });
+
