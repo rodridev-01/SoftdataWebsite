a6774716c802d496ff9b6f347accfccb72040d14
XrL8D <rph26031994@gmail.com>
2024-06-03 02:09:31 -0500

    patch

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="astro/client" />
 MONGO_DB_PASS=c6LlVaD0h272w1WD
 PORT=3000
-MONGODB_URI=mongodb+srv://<usuario>:<contraseña>@<cluster>/<basededatos>?retryWrites=true&w=majority
