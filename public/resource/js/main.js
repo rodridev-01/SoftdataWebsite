b7b4157caa3726867313872912169b733e2eaece
XrL8D <rph26031994@gmail.com>
2024-06-09 23:36:41 -0500

    rework

--- public/resource/js/main.js
+++ public/resource/js/main.js
@@ -1,77 +1,69 @@
-/*=============== SEARCH ===============*/
-const searchButton = document.getElementById('search-button'),
-      searchClose = document.getElementById('search-close'),
-      searchContent = document.getElementById('search-content')
-
-/* Menu show */
-if(searchButton){
-    searchButton.addEventListener('click', () =>{
-        searchContent.classList.add('show-search')
-    })
-}
-
-/* Menu hidden */
-if(searchClose){
-    searchClose.addEventListener('click', () =>{
-        searchContent.classList.remove('show-search')
-    })
-}
-/*=============== LOGIN ===============*/
-const loginButton = document.getElementById('login-button'),
-      loginClose = document.getElementById('login-close'),
-      loginContent = document.getElementById('login-content')
-
-/* Menu show */
-if(loginButton){
-    loginButton.addEventListener('click', () =>{
-        loginContent.classList.add('show-login')
-    })
-}
-
-/* Menu hidden */
-if(loginClose){
-    loginClose.addEventListener('click', () =>{
-        loginContent.classList.remove('show-login')
-    })
-}
-/*=============== REGISTER ===============*/
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
-
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
-/*=============== ADD SHADOW HEADER ===============*/
-const shadowHeader = () =>{
-    const header = document.getElementById('header')
-    // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('shadow-header') 
-                       : header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll', shadowHeader)
-
-/*=============== HOME SWIPER ===============*/
-let swiperHome = new Swiper('.home__swiper', {
-    loop: true,
-    spaceBetween: -24,
-    grabCursor:true,
-    slidesPerView:'auto',
-    centeredSlides:'auto',
-
-    autoplay: {
-        delay:3000,
-        disableOnInteraction: false,
-    },
-
-    breakpoints:{
-        1220:{
-            spaceBetween:-32,
-        }
+function openNav(){
+    'use strict';
+    const sidepanel=document.getElementById('mySidepanel');
+    if(sidepanel){
+        sidepanel.style.left=0;
+    }else{
+        console.error('error:side panel not found')
     }
-  });
+}
 
+function closeNav(){
+    'use strict';
+    const sidepanel=document.getElementById('mySidepanel');
+    if(sidepanel){
+        sidepanel.style.left='-320px';
+    }else{
+        console.error('error:side panel not found')
+    }
+}
+// portfolio gallary tab
+function open_img(evt, cityName){
+    let i, tabcontent, tablinks;
+
+    //hide all tab content
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for(i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none"
+    }
+
+    //remove active class from all tab links
+    tablinks = document.getElementsByClassName('tablinks');
+    for(i = 0; i < tablinks.length; i++){
+        tablinks[i].classList.remove("active")
+    }
+
+    // show the selected tab content and mark the corresponding tab link asa active
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.classList.add('active');
+}
+    
+
+
+//reponsive logoipsum slider
+$('.sliderlogo').slick({
+    arrows: false,
+    dots: false,
+    infinite: false,
+    autoplay: false,
+    speed: 300,
+    slidesToShow: 5,
+    sliderToScroll: 1,
+    reponsive:[{
+        breakpoint:1024,
+        settings:{
+            slidesToShow: 4,
+            sliderToScroll: 1,
+            infinite: true,
+            dots: false,
+        }
+    },
+    {
+        breakpoint:600,
+        settings:{
+            slidesToShow:2,
+            sliderToScroll:1
+        }
+    },
+    ]
+});
