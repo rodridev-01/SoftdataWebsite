fde729f30c9053ae5ce0bbf73f8349a3c8bcc239
XrL8D <rph26031994@gmail.com>
2024-06-11 03:11:56 -0500

    rework

--- public/resource/js/main.js
+++ public/resource/js/main.js
@@ -67,3 +67,11 @@ $('.sliderlogo').slick({
     },
     ]
 });
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
